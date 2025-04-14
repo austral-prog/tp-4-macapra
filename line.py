cbb0007b3aa5cb883ac0751f289c3577597ebf00
macapra <mcapraprince@mail.austral.edu.ar>
2025-04-14 14:47:24 -0300

    Update line.py

--- line.py
+++ line.py
@@ -3,11 +3,11 @@ def line():
     B = float((input("Ingrese el coeficiente de B: ")))
     X1 = float((input("Ingrese el coeficiente de X1: ")))
     X2 = float((input("Ingrese el coeficiente de X2: ")))
-    print(f"El coeficiente A de su ecuacion de la recta es: {A}")
-    print(f"El coeficiente B de su ecuacion de la recta es: {B}")
-    print(f"El coeficiente X1 de su ecuacion de la recta es: {X1}")
-    print(f"El coeficiente X2 de su ecuacion de la recta es: {X2}")
-    print(f"\nPara la siguiente ecuacion: \n\tY= {A}X + {B} ")
+    print(f"El coeficiente A de su ecuación de la recta es: {A}")
+    print(f"El coeficiente B de su ecuación de la recta es: {B}")
+    print(f"El coeficiente X1 de su ecuación de la recta es: {X1}")
+    print(f"El coeficiente X2 de su ecuación de la recta es: {X2}")
+    print(f"\nPara la siguiente ecuación: \n\tY= {A}X + {B} ")
     Y1 = A * X1 + B
     Y2 = A * X2 + B
     print(f"\nDado los siguientes puntos: \n\tP1 ({X1}, {Y1})\n\tP2 ({X2}, {Y2})")
